81097e7f45489b561618b1c3cc7a5e35b6707f83
Gus <gus.ryan163@gmail.com>
2018-06-26 17:16:49 +0800

    Formatting

--- stringreverse.cpp
+++ stringreverse.cpp
@@ -3,24 +3,29 @@
 
 // More "C++"ey way of doing it.
 std::string reverseString(std::string s) {
-    if (s.length() > 1) {
+    if (s.size() > 1) {
         std::string::iterator start = s.begin(), end = --s.end();
         while (start < end) {
             std::swap(*start++, *end--);
             ++start, --end;
         }
     }
+    
     return std::move(s);
 }
 
 // More "C"ey way of doing it.
 // Inline swap with no temp value done using bitwise XOR.
 std::string reverseStringInlineSwap(std::string s) {
-    for (int i = 0; i < s.size()/2; i++) {
-        s[i] ^= s[s.size() - i-1];
-        s[s.size() - i-1] ^= s[i];
-        s[i] ^= s[s.size() - i-1];
+    const int& length = s.size();
+    if (length > 1) {
+        for (int i = 0; i < length/2; i++) {
+            s[i] ^= s[length - i-1];
+            s[length - i-1] ^= s[i];
+            s[i] ^= s[length - i-1];
+        }
     }
+
     return std::move(s);
 }
 
