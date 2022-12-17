05f682010d17eb1984e9c8ed54e82326767365a8
Gus Ryan <gus.ryan163@gmail.com>
2022-12-17 00:33:06 +0000

    remove unnecessary array

--- src/ts/sudokuSolver.ts
+++ src/ts/sudokuSolver.ts
@@ -30,16 +30,6 @@ function solveSudoku(board: Board): void {
 		squareValidNums[square] = bitUnset(squareValidNums[square], num)
 	}
 
-	// function to find all the valid numbers in the current position on the board
-	// assumes that the current spot is empty
-	function findValidNumbers(i: number, j: number, square: number): number[] {
-		const entries: number[] = []
-		const bit = rowValidNums[j] & columnValidNums[i] & squareValidNums[square]
-		for (let num = 1; num < 10; ++num) if ((1 << num) & bit) entries.push(num)
-
-		return entries
-	}
-
 	function dfs(index: number): boolean {
 		if (index === 81) return true
 
@@ -47,9 +37,12 @@ function solveSudoku(board: Board): void {
 		// skip to the next spot if the number is already set
 		if (board[i][j] !== ".") return dfs(index + 1)
 
-		const validNumbers = findValidNumbers(i, j, square)
+		// bitmask corresponding to all the valid numbers for the current position on the board
+		const bitmask =
+			rowValidNums[j] & columnValidNums[i] & squareValidNums[square]
+		for (let num = 1; num < 10; ++num) {
+			if (!((1 << num) & bitmask)) continue
 
-		for (const num of validNumbers) {
 			// set the current position to the first valid number
 			board[i][j] = num.toString() as Entry
 			rowValidNums[j] = bitUnset(rowValidNums[j], num)
